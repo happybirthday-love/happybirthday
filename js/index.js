2d8c2cfbce693ef40df29a8bc42c23f8096ba09a
Amo James <james@localhost.localdomain>
2020-04-21 23:50:47 +0000

    update

--- js/index.js
+++ js/index.js
@@ -301,21 +301,3 @@ function reveal() {
   document.querySelector('#image').appendChild(ifrm);
 }
 
-window.addEventListener('load', function () {
-        var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-        var source = audioCtx.createBufferSource();
-        var xhr = new XMLHttpRequest();
-        xhr.open('GET', '../music/(1497) Jon Bellion ft. Travis Mendes - Guillotine (Official Audio) - YouTube.MP3');
-        xhr.responseType = 'arraybuffer';
-        xhr.addEventListener('load', function (r) {
-            audioCtx.decodeAudioData(
-                    xhr.response,
-                    function (buffer) {
-                        source.buffer = buffer;
-                        source.connect(audioCtx.destination);
-                        source.loop = false;
-                    });
-            source.start(0);
-        });
-        xhr.send();
-    });
